d3fdbf349fcbfd6d46aa795d763f54bf11d9cd41
Carlos <Carlos@ESCRITORIO>
2016-11-01 20:01:10 -0200

    Add project files.

---
+++ Modelos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Modelos.Startup))]
+namespace Modelos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
